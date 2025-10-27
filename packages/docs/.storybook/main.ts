bcd3d5d584cf1bca7bc7fead661789b9922f24f7
Joel <joelluis73@gmail.com>
2025-10-27 11:22:02 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,19 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { StorybookConfig } from '@storybook/react-vite';
-
-import { join, dirname } from "path"
+import { join, dirname } from 'path';
 
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
+  return dirname(require.resolve(join(value, 'package.json')));
 }
+
 const config: StorybookConfig = {
-  "stories": [
-    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-  ],
-  "addons": [],
-  "framework": {
-    "name": getAbsolutePath('@storybook/react-vite'),
-    "options": {}
-  }
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [],
+  framework: {
+    name: getAbsolutePath('@storybook/react-vite'),
+    options: {},
+  },
+  viteFinal: async (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/github-users-monorepo/';
+    }
+
+    return config;
+  },
 };
+
 export default config;
